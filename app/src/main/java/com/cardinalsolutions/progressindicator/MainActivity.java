c6954eab4e081c1f7dfaa5c10df34d90c2325407
Jacob Kanipe-Illig <jkanipe-illig@cardinalsolutions.com>
2016-06-15 16:43:04 -0400

    Update project configuration and remove dependency on Android Autowire.

--- app/src/main/java/com/cardinalsolutions/progressindicator/MainActivity.java
+++ app/src/main/java/com/cardinalsolutions/progressindicator/MainActivity.java
@@ -2,26 +2,17 @@ package com.cardinalsolutions.progressindicator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import com.cardinalsolutions.android.arch.autowire.AndroidLayout;
-import com.cardinalsolutions.android.arch.autowire.AndroidView;
-
-@AndroidLayout(R.layout.activity_main)
-public class MainActivity extends BaseActivity {
-
-	@AndroidView(R.id.circle_progress_bar)
-	private ProgressBar mProgress;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-	    mProgress.setProgress(65);
+        ProgressBar progress = (ProgressBar) findViewById(R.id.circle_progress_bar);
+	    progress.setProgress(33);
 
     }
 
